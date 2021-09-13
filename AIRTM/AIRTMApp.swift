19718f36633ef0f3593b92544b3737070d5e45cc
jas21068 <83102649+jas21068@users.noreply.github.com>
2021-09-13 14:40:52 -0700

    Initial Commit

---
+++ AIRTM/AIRTMApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIRTMApp.swift
+//  AIRTM
+//
+//  Created by Jaskirat Mangat on 2021-09-13.
+//
+
+import SwiftUI
+
+@main
+struct AIRTMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
